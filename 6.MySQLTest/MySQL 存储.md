0e7a4611226fc4594ef8c71939dce4fe1e82c931
quanchenliu <aimat2020@163.com>
2024-02-06 22:45:33 +0800

    update

---
+++ 6.MySQLTest/MySQL 存储.md
@@ -0,0 +1,11 @@
+# MySQL 存储
+
+
+
+### 一、MySQL 的安装的使用教程
+
+[2023 年 MySQL 8.0 安装配置 最简易（保姆级）_mysql安装-CSDN博客](https://blog.csdn.net/m0_52559040/article/details/121843945)
+
+
+
+## 二、
--- MySQLTest-master/demo1.py
+++ MySQLTest-master/demo1.py
@@ -1,12 +1,12 @@
 """
  -*- coding : utf-8 -*-
  @Author  	: quanchenliu
- @Time	   	: 2024/2/4
- @Function  : 爬取网页数据，并以 .txt 格式存储（open、write、close）
+ @Time	   	: 2024/2/6
+ @Function  :
 """
 import pymysql
 
-db = pymysql.connect(host='localhost', user='root', password=None, port=3306)
+db = pymysql.connect(host='localhost', user='root', password='190901sjnh', port=3306)
 cursor = db.cursor()
 cursor.execute('SELECT VERSION()')
 data = cursor.fetchone()
