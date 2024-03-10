45b4a0f5382b63f1771b76dc9e14220e88128896
quanchenliu <aimat2020@163.com>
2024-03-10 18:05:34 +0800

    update

--- 项目实战训练/b站视频弹幕爬虫+情感分析+词云/1_B站弹幕爬虫.py
+++ 项目实战训练/b站视频弹幕爬虫+情感分析+词云/1_B站弹幕爬虫.py
@@ -1,24 +1,26 @@
-# 程序功能：爬取B站视频弹幕
-# 作者：马哥python说
-
+"""
+ -*- coding : utf-8 -*-
+ @Author  	: quanchenliu
+ @Time	   	: 2024/3/10
+ @Function  : B站弹幕爬虫
+"""
 import re  # 正则表达式提取文本
-import requests  # 爬虫发送请求
+import requests
 from bs4 import BeautifulSoup as BS  # 爬虫解析页面
 import time
 import pandas as pd  # 存入csv文件
 import os
 
 
-def get_bilibili_danmu(v_url, v_result_file):
-	"""
-	爬取B站弹幕
-	:param v_url: 视频地址
-	:param v_result_file: 保存文件名
-	:return:
-	"""
-	headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)", }
-	print('视频地址是：', v_url)
-	r1 = requests.get(url='https://api.bilibili.com/x/player/pagelist?bvid='+bv, headers=headers)
+
+"""爬取B站弹幕: param v_url: 视频地址, param v_result_file: 保存文件名, return:"""
+def get_bilibili_danmu(bv, url, result_file):
+	headers = {
+		'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)"
+	}
+	print('视频地址是：', url)
+
+	r1 = requests.get(url='https://api.bilibili.com/x/player/pagelist?bvid=' + bv, headers=headers)
 	html1 = r1.json()
 	cid = html1['data'][0]['cid']  # 获取视频对应的cid号
 	print('该视频的cid是:', cid)
@@ -37,7 +39,7 @@ def get_bilibili_danmu(v_url, v_result_file):
 		data_split = d['p'].split(',')  # 按逗号分隔
 		temp_time = time.localtime(int(data_split[4]))  # 转换时间格式
 		danmu_time = time.strftime("%Y-%m-%d %H:%M:%S", temp_time)
-		video_url_list.append(v_url)
+		video_url_list.append(url)
 		danmu_url_list.append(danmu_url)
 		time_list.append(danmu_time)
 		text_list.append(d.text)
@@ -47,25 +49,28 @@ def get_bilibili_danmu(v_url, v_result_file):
 	df['弹幕地址'] = danmu_url_list
 	df['弹幕时间'] = time_list
 	df['弹幕内容'] = text_list
-	if os.path.exists(v_result_file):  # 如果文件存在，不需写入字段标题
+	if os.path.exists(result_file):  # 如果文件存在，不需写入字段标题
 		header = None
 	else:  # 如果文件不存在，说明是第一次新建文件，需写入字段标题
 		header = ['视频地址', '弹幕地址', '弹幕时间', '弹幕内容']
-	df.to_csv(v_result_file, encoding='utf_8_sig', mode='a+', index=False, header=header)  # 数据保存到csv文件
+	df.to_csv(result_file, encoding='utf_8_sig', mode='a+', index=False, header=header)  # 数据保存到csv文件
 
+def main():
+	csv_file = '刘畊宏弹幕.csv'  				# 保存数据的文件名
 
-if __name__ == "__main__":
-	print('爬虫程序开始执行！')
-	# 保存数据的文件名
-	csv_file = '刘畊宏弹幕.csv'
-	# 如果存在csv文件，先删除，避免数据重复
-	if os.path.exists(csv_file):
+	if os.path.exists(csv_file):  			# 如果存在csv文件，先删除，避免数据重复
 		print('{}已存在，开始删除文件'.format(csv_file))
 		os.remove(csv_file)
 		print('{}已删除文件'.format(csv_file))
+
 	# "刘畊宏"弹幕数较多的视频Bv号
 	bv_list = ['BV1Pa411v7vg', 'BV1t3411T7k6', 'BV1wY411P7cV', 'BV1fr4y1J7tL', 'BV1o44y1N7X9']
+
 	# 开始爬取
 	for bv in bv_list:
-		get_bilibili_danmu(v_url='https://www.bilibili.com/video/{}'.format(bv), v_result_file='刘畊宏弹幕.csv')
+		get_bilibili_danmu(bv, url='https://www.bilibili.com/video/{}'.format(bv), result_file='刘畊宏弹幕.csv')
+
+if __name__ == "__main__":
+	print('爬虫程序开始执行！')
+	main()
 	print('爬虫程序执行完毕！')
